917d61b7f8c53fcf27c4bb3c865e297246ca680f
Curt Wilhelm <curt@rightscale.com>
2018-07-31 12:55:49 -0600

    complete instances.sh

--- weekly_ri_report/instances.sh
+++ weekly_ri_report/instances.sh
@@ -5,8 +5,8 @@ set -x
 
 #defaults
 API_HOST="us-4"
-REFRESH_TOKEN=""#"7b9dc17b8ec3fbc971fa68953e5b227f1a3ebe04"
-START_TIME="2018-7-15T00:00:00"
+REFRESH_TOKEN=""
+START_TIME="2018-015-01T00:00:00"
 END_TIME="2018-07-30T23:59:00"
 ACCOUNT_IDS="" #"106220"
 TIMEZONE="America/Los_Angeles"
@@ -71,38 +71,49 @@ done
 
 my_token_endpoint="https://$API_HOST.rightscale.com/api/oauth2"
 
+# create JSON array of instance_filters for export
 declare -a instance_filters
-
-for a in ${ACCOUNT_IDS[@]} ; do
-  item="{\"type\":\"instance:account_id\",\"value\":\"$a\"}"
-  instance_filters=("${instance_filters[@]}" $item)
+for (( i=0; i<${#ACCOUNT_IDS[@]}; i++ ));do
+  item="{\"type\":\"instance:account_id\",\"value\":\"${ACCOUNT_IDS[$i]}\"}"
+  instance_filters=(${instance_filters[@]} $item)
 done
-instance_filters_json=$(printf '%s\n' "${instance_filters[@]}" | jq -R . | jq -s .)
-echo $instance_filters_json
+instance_filters_json=$(printf '%s\n' ${instance_filters[@]} | jq -R . | jq -s .)
+instance_filters_json=$(eval echo $instance_filters_json)
+
 #main
 # get access_token
-if [ -n $REFRESH_TOKEN ];then
+if [ -n "$REFRESH_TOKEN" ];then
   access_token=$(curl -H "X-API-Version:1.5" --request POST "$my_token_endpoint"  \
        -d "grant_type=refresh_token"  -d "refresh_token=$REFRESH_TOKEN" | jq -r '.access_token')
   AUTH_OPTION="-H \"Authorization: Bearer $access_token\""
-  #AUTH_OPTION=$(eval echo $AUTH_OPTION)
-  AUTH_OPTION=$(printf "%s" "${AUTH_OPTION}")
+  #AUTH_OPTION=$(echo -e ${AUTH_OPTION})
+  AUTH_OPTION=$(printf "%s" -H "Authorization:Bearer $access_token")
 else
+  if [ -z "$EMAIL" ];then
+    echo "Refresh token or Email and/or Password is missing."
+    help
+    exit 1
+  fi
   curl -i -H X_API_VERSION:1.5 -c mycookie -X POST --data-urlencode "email=$EMAIL" \
   --data-urlencode "password=$PASSWORD" -d account_href=/api/accounts/"$account" https://$API_HOST.rightscale.com/api/session
-  AUTH_OPTION=" -b mycookie "
+  AUTH_OPTION=$(printf "%s" "-b mycookie")
 fi
 
+if [ -z "$ACCOUNT_IDS[@]" ];then
+  echo "missing account id's to filter"
+  help
+  exit 1
+fi
 # # export instances
-curl -v -H "X-API-Version:1.0" -H "Authorization: Bearer $access_token"  \
+curl -H "X-API-Version:1.0" $AUTH_OPTION  \
   -H Content-Type:text/json \
   -o instances-$TIMESTAMP.csv \
   -d "{\"start_time\":\"$START_TIME\",\"end_time\":\"$END_TIME\",\"timezone\":\"$TIMEZONE\",\"instance_filters\":$instance_filters_json}"\
   https://analytics.rightscale.com/api/instances/actions/export
 
-# #export reserved_instances
-# curl  -H "X-API-Version:1.0" -H "Authorization: Bearer $access_token"  \
-#   -H Content-Type:text/json \
-#   -o ri-$TIMESTAMP.csv \
-#   -d "{\"start_time\": \"$START_TIME\",\"end_time\": \"$END_TIME\",\"timezone\": \"$TIMEZONE\",\"reserved_instance_filters\": [{\"type\": \"instance:account_id\",\"value\": \"106220\"}]}"\
-#   https://analytics.rightscale.com/api/reserved_instances/actions/export
+#export reserved_instances
+curl  -H "X-API-Version:1.0" $AUTH_OPTION  \
+  -H Content-Type:text/json \
+  -o ri-$TIMESTAMP.csv \
+  -d "{\"start_time\":\"$START_TIME\",\"end_time\":\"$END_TIME\",\"timezone\":\"$TIMEZONE\",\"reserved_instance_filters\":$instance_filters_json}"\
+  https://analytics.rightscale.com/api/reserved_instances/actions/export
